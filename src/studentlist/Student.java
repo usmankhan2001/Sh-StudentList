fd5c9859a24561faa53278c848f5483a7c2b489b
usmankhan2001 <usmanafzal.khan@gmail.com>
2020-05-29 16:32:13 -0400

    Added getter & setter for id field

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -2,6 +2,16 @@ package studentlist;
 public class Student {
     
     private String name;
+    
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public Student(String name) {
         this.name = name;
